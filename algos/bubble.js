cdb6879e95bb683e582fb94d3a573ce5fa5c7c8a
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2026-04-19 15:46:37 +0100

    Update bubble.js

--- algos/bubble.js
+++ algos/bubble.js
@@ -1,4 +1,4 @@
-async function bubbleSort(arr, id) {
+export async function bubbleSort(arr, id) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
