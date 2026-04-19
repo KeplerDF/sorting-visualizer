3229dd408aeacc5cf1d8861b8e725dd886618ee1
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2026-04-19 16:17:37 +0100

    Update bubble.js

--- algos/bubble.js
+++ algos/bubble.js
@@ -1,5 +1,4 @@
-import { render } from '../main.js';
-import { state, wait } from '../controller.js';
+import { state, wait, render } from '../controller.js';
 
 export async function bubbleSort(arr, id) {
     for (let i = 0; i < arr.length; i++) {
