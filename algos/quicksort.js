8e89b2a03d534a745aac331f9e3247d53771b1a2
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2026-04-19 21:58:45 +0100

    Update quicksort.js

--- algos/quicksort.js
+++ algos/quicksort.js
@@ -1,26 +1,26 @@
 import { state, wait, render } from '../controller.js';
 import { playNote } from '../audio.js';
 
-export async function quickSort(arr, left, right, containerId) {
-    if (state.isResetting) return;
-    if (left >= right) return;
-    let index = await partition(arr, left, right, containerId);
+export async function quickSort(arr, left, right, containerId, mySteps) {
+    if (!mySteps) mySteps = { count: state.stepCount };
+
+    if (state.isResetting || left >= right) return;
+
+    let index = await partition(arr, left, right, containerId, mySteps);
+    
     await Promise.all([
-        quickSort(arr, left, index - 1, containerId),
-        quickSort(arr, index + 1, right, containerId)
+        quickSort(arr, left, index - 1, containerId, mySteps),
+        quickSort(arr, index + 1, right, containerId, mySteps)
     ]);
 }
 
-export async function partition(arr, left, right, containerId) {
+export async function partition(arr, left, right, containerId, mySteps) {
     
-    // Change 'high' to 'right' and 'low' to 'left'
     let pivot = arr[right]; 
     let i = left - 1;
-    let mySteps = { count: state.stepCount };
 
     for (let j = left; j <= right - 1; j++) {
         if (state.isResetting) return;
-        // Highlight current bar (j), pivot (right), and i
         render(arr, containerId, [j, right, i]);
         playNote(arr[j], 'square');
         await wait(mySteps);
